ab3aa3c1b2333b989be5d5e082b98db214316d03
kummathisrujana <73153127+kummathisrujana@users.noreply.github.com>
2020-10-20 10:39:47 +0530

    java.txt

---
+++ ArmstrongExample.java
@@ -0,0 +1,19 @@
+class ArmstrongExample
+{
+public static void main(String[]args)
+{
+int a,temp,c=0;
+int n=153;
+temp=n;
+while(n>0)
+{
+a=n%10;
+n=n/10;
+c=c+(a*a*a);
+}
+if(temp==c)
+{
+System.out.println("n is armstrong");
+}
+}
+}
